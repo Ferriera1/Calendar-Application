fda09ebdfcaec4c04d1cf9fe5b16f1737f359d95
Mona Lisa <ferrieradayna@gmail.com>
2023-08-28 18:26:23 -0400

    [added some code to all files]

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,26 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+$(document).ready(function(){
+var currentDate = new Date();
+var now = currentDate.getHours() + ":" + currentDate.getMinutes() + ":" + currentDate.getSeconds();
+console.log(now);
+//var now = dayjs("MMMM Do YYYY, h:mm:ss a");
+//console.log (dayjs().hour());
+});
+
+
+var timeElement = document.getElementById("headerClock");
+
+function updateTime() {
+  var now = new Date();
+
+  timeElement.innerText = now;
+}
+
+updateTime();
+setInterval(updateTime, 1000);
+
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -14,6 +34,12 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
+
+
+  // while (now == true) {
+    
+  // }
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Calendar-Application
 Added some code? not sure if its correct lol. Will double back
+^ code was correct 
+Working on the HTML first, I added the different divs to represent the different hour blocks
